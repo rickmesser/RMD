9f13cc7e12bc14e48726dc79e892db380debde37
rick <rick@rickmesser.com>
2013-02-27 12:46:40 -0600

    Version 2

---
+++ _/js/main.js
@@ -0,0 +1,6 @@
+// Swipe Elements
+
+new Swipe(document.getElementById('slider'));
+new Swipe(document.getElementById('slider2'));
+var slider3 = new Swipe(document.getElementById('slider3'));
+var slider4 = new Swipe(document.getElementById('slider4'));
