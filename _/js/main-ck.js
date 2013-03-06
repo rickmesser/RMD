ed1e1d60aa1f0728ea2f0a87aa2fed663980c9e9
rick <rick@rickmesser.com>
2013-03-06 17:00:30 -0600

    Update icon font, update bootstrap

--- _/js/main-ck.js
+++ _/js/main-ck.js
@@ -1,10 +1,7 @@
 // Swipe Elements
-
 // new Swipe(document.getElementById('slider'));
 // new Swipe(document.getElementById('slider2'));
 // var slider3 = new Swipe(document.getElementById('slider3'));
 // var slider4 = new Swipe(document.getElementById('slider4'));
-
 // FitText 
-$("#tt").fitText(1.1, { minFontSize: '100px', maxFontSize: '200px' });
-$("#message").fitText(1.1, { minFontSize: '16px', maxFontSize: '36px' });
+$("#tt").fitText(1.1,{minFontSize:"100px",maxFontSize:"200px"});$("#message").fitText(1.1,{minFontSize:"16px",maxFontSize:"36px"});
