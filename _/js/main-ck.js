cf54f40b885dd28bcc2223cda8b36f065bcd728b
Rick Messer <rick@rickmesser.com>
2013-03-10 19:46:17 -0500

    Add Bootstrap Image Gallery plugin

--- _/js/main-ck.js
+++ _/js/main-ck.js
@@ -1,7 +1,11 @@
-// Swipe Elements
-// new Swipe(document.getElementById('slider'));
-// new Swipe(document.getElementById('slider2'));
-// var slider3 = new Swipe(document.getElementById('slider3'));
-// var slider4 = new Swipe(document.getElementById('slider4'));
-// FitText 
-$("#tt").fitText(1.1,{minFontSize:"100px",maxFontSize:"200px"});$("#message").fitText(1.1,{minFontSize:"16px",maxFontSize:"36px"});
+// Bootstrap Image Gallery
+/*
+ * Bootstrap Image Gallery JS Example 2.9
+ * https://github.com/blueimp/Bootstrap-Image-Gallery
+ *
+ * Copyright 2012, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ *//*jslint unparam: true *//*global window, document, $ */$(function(){"use strict";$("#start-slideshow").button().click(function(){var e=$(this).data(),t=$(e.target),n=t.data("modal");n?$.extend(n.options,e):e=$.extend(t.data(),e);t.find(".modal-slideshow").find("i").removeClass("icon-play").addClass("icon-pause");t.modal(e)});$("#toggle-fullscreen").button().click(function(){var e=$(this),t=document.documentElement;if(!e.hasClass("active")){$("#modal-gallery").addClass("modal-fullscreen");t.webkitRequestFullScreen?t.webkitRequestFullScreen(window.Element.ALLOW_KEYBOARD_INPUT):t.mozRequestFullScreen&&t.mozRequestFullScreen()}else{$("#modal-gallery").removeClass("modal-fullscreen");(document.webkitCancelFullScreen||document.mozCancelFullScreen||$.noop).apply(document)}})});$("#tt").fitText(1.1,{minFontSize:"100px",maxFontSize:"200px"});$("#message").fitText(1.1,{minFontSize:"16px",maxFontSize:"36px"});
